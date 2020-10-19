43e58f8eba623542d42b0b401f3bae8ac94f400d
Heytor Victor <heytorvas@gmail.com>
2020-10-18 22:49:58 -0300

    update

--- redes-neurais/question13.py
+++ redes-neurais/question13.py
@@ -1,25 +1,4 @@
-def output(d):
-	if d >= 0:
-		return 1
-	return -1
-
-def individual_error(o, f):
-	return o - f
-
-def sum(w, x):
-	d = w[0]
-	for i in range(2):
-		d += w[i+1]*x[i]
-	return d
-
-def summation(numbers):
-    sum = 0
-    for i in numbers:
-        sum = sum + i
-    return sum
-
-def global_error(e):
-    return 0.5*summation(e) ** 2
+from util import output, individual_error, sum, summation, global_error
 
 x1 = [1,0,4,5,1,6]
 x2 = [1,2,3,4,0,4]
